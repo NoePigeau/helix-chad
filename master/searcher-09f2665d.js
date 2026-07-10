dc594a5797195b9b109766bd490e014f2e77d3f4
NoePigeau <NoePigeau@users.noreply.github.com>
2026-07-10 14:05:49 +0000

    deploy: 4ffa0ac283c85aea9696de54381ea65a18e758e9

--- master/searcher-09f2665d.js
+++ master/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-28224a4d.js',
+                path_to_root + 'searchindex-3a374221.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
